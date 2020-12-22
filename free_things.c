f77210e7fdb7feb746613511dd8f1c85a42037e3
kwhit2 <kfw2@outlook.com>
2020-12-22 18:35:40 +0000

    Betty

--- free_things.c
+++ free_things.c
@@ -2,15 +2,15 @@
 
 /**
  * free_things - frees things
- * @stack:
- * @line_number:
+ * @stack: stack
+ * @line_number: line number
  * Return: void
  */
 
 void free_things(stack_t **stack, unsigned int line_number)
 {
-    if (stack == NULL)
-        return;
-    while (*stack != NULL)
-        pop(stack, line_number);
+	if (stack == NULL)
+		return;
+	while (*stack != NULL)
+		pop(stack, line_number);
 }
