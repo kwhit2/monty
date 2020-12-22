8afb85374969e4c2ef4096cd879876ba0398356e
laurendobratz00 <laurendobratz@icloud.com>
2020-12-22 15:55:33 +0000

    changed back

--- _operations.c
+++ _operations.c
@@ -95,9 +95,9 @@ void pall(stack_t **stack, unsigned int line_number)
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
+	(void)line_number;
 
-	fprintf(stderr, "L%d: does absolutely nothing\n", line_number
-		;
+	;
 }
 
 /**
