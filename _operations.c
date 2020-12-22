72604f9bcacb6831c601944dcc34588b2ab7024b
kwhit2 <kfw2@outlook.com>
2020-12-22 02:31:00 +0000

    Printing to stderr

--- _operations.c
+++ _operations.c
@@ -11,7 +11,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		printf("L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->next != NULL) /* continue until end of stack */
